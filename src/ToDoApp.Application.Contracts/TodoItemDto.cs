14ea5387ec4d6aa5cd045ef3bdb86de64e18b240
habibalt <hbaltaci@agdas.com.tr>
2022-10-30 23:23:35 +0300

    Add project files.

---
+++ src/ToDoApp.Application.Contracts/TodoItemDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp
+{
+    public class TodoItemDto:IValidatableObject
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(1000)]
+        public string Text { get; set; }
+        public IEnumerable<ValidationResult> Validate(
+           ValidationContext validationContext)
+        {
+            if (Text == "a")
+            {
+                yield return new ValidationResult(
+                    "Boş yapılacak bişi mi var",
+                    new[] { "Text" }
+                );
+            }
+        }
+    }
+}
---
+++ src/ToDoApp.Application/ToDoAppAppService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Localization;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace ToDoApp;
+
+/* Inherit your application services from this class.
+ */
+ public  class ToDoAppAppService : ApplicationService,ITodoAppService
+{
+    private readonly IRepository<ToDoItem, Guid> _todoItemRepository;
+    public ToDoAppAppService(IRepository<ToDoItem,Guid> todoItemRepository)
+    {
+        LocalizationResource = typeof(ToDoAppResource);
+        _todoItemRepository = todoItemRepository;
+    }
+
+    public async Task<List<TodoItemDto>> GetListAsync()
+    {
+        var items = await _todoItemRepository.GetListAsync();
+        return items
+            .Select(item => new TodoItemDto
+            {
+                Id = item.Id,
+                Text = item.Text
+            }).ToList();
+    }
+    public async Task<TodoItemDto> CreateAsync(string text)
+    {
+        var todoItem = await _todoItemRepository.InsertAsync(
+            new ToDoItem { Text = text }
+        );
+
+        return new TodoItemDto
+        {
+            Id = todoItem.Id,
+            Text = todoItem.Text
+        };
+    }
+    public async Task DeleteAsync(Guid id)
+    {
+        await _todoItemRepository.DeleteAsync(id);
+    }
+
+}
